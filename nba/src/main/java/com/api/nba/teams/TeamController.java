df051a3ba7190d08073ab665499b974e30fb19ea
Petarj123 <pjankovic03@gmail.com>
2023-07-17 16:04:08 +0200

    New methods

--- nba/src/main/java/com/api/nba/teams/TeamController.java
+++ nba/src/main/java/com/api/nba/teams/TeamController.java
@@ -1,8 +1,6 @@
 package com.api.nba.teams;
 
-import com.api.nba.DTO.ConferenceTeams;
-import com.api.nba.DTO.DivisionTeams;
-import com.api.nba.DTO.Teams;
+import com.api.nba.DTO.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +26,25 @@ public class TeamController {
     @ResponseStatus(HttpStatus.OK)
     public DivisionTeams getDivisionTeams(@RequestParam String division){
         return teamService.getDivisionTeams(division);
+    }
+    @GetMapping("/standings")
+    @ResponseStatus(HttpStatus.OK)
+    public StandingsSimple getStandings(@RequestParam String season){
+        return teamService.getStandings(season);
+    }
+    @GetMapping("/standings/conference")
+    @ResponseStatus(HttpStatus.OK)
+    public DivisionRankings getConferenceStandings(@RequestParam String season){
+        return teamService.getConferenceStandings(season);
+    }
+    @GetMapping("/standings/divisions")
+    @ResponseStatus(HttpStatus.OK)
+    public DivisionRankings getDivisionStandings(@RequestParam String season){
+        return teamService.getDivisionStandings(season);
+    }
+    @GetMapping("/h2h")
+    @ResponseStatus(HttpStatus.OK)
+    public HeadToHead getDivisionStandings(@RequestParam String team1, @RequestParam String team2){
+        return teamService.getHeadToHeadScore(team1, team2);
     }
 }
--- nba/src/main/java/com/api/nba/teams/TeamService.java
+++ nba/src/main/java/com/api/nba/teams/TeamService.java
@@ -1,18 +1,23 @@
 package com.api.nba.teams;
 
-import com.api.nba.DTO.ConferenceTeams;
-import com.api.nba.DTO.DivisionTeams;
-import com.api.nba.DTO.Teams;
+import com.api.nba.DTO.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class TeamService {
 
     private final ConferenceRepository conferenceRepository;
+    private final StandingsPerSeasonRepository standingsPerSeasonRepository;
+    private final TeamVsTeamRepository teamVsTeamRepository;
+
+
+
 
     public Teams getAllTeams() {
         List<Conference> teams = conferenceRepository.findAll();
@@ -29,14 +34,69 @@ public class TeamService {
     }
     public DivisionTeams getDivisionTeams(String division) {
         List<String> teams = switch (division.toLowerCase()) {
-            case "atlantic" -> conferenceRepository.findAtlanticConferenceTeamNames();
-            case "central" -> conferenceRepository.findCentralConferenceTeamNames();
-            case "southeast" -> conferenceRepository.findSouthEastConferenceTeamNames();
-            case "southwest" -> conferenceRepository.findSouthWestConferenceTeamNames();
-            case "northeast" -> conferenceRepository.findNorthEastConferenceTeamNames();
-            case "northwest" -> conferenceRepository.findNorthWestConferenceTeamNames();
+            case "atlantic" -> conferenceRepository.findAtlanticDivisionTeamNames();
+            case "central" -> conferenceRepository.findCentralDivisionTeamNames();
+            case "southeast" -> conferenceRepository.findSouthEastDivisionTeamNames();
+            case "southwest" -> conferenceRepository.findSouthWestDivisionTeamNames();
+            case "northwest" -> conferenceRepository.findNorthWestDivisionTeamNames();
+            case "pacific" -> conferenceRepository.findPacificDivisionTeamNames();
             default -> throw new IllegalArgumentException("Invalid division");
         };
         return new DivisionTeams(division, teams);
     }
+    public StandingsSimple getStandings(String season){
+        List<StandingsPerSeason> list = standingsPerSeasonRepository.findBySeason(season);
+        List<Ranking> resultList = list.stream().map(team -> new Ranking(team.getSeed(), team.getTeam(), team.getOverall())).toList();
+        return new StandingsSimple(season, resultList);
+    }
+    public DivisionRankings getConferenceStandings(String season){
+        List<StandingsPerSeason> list = standingsPerSeasonRepository.findBySeason(season);
+
+        Map<String, Supplier<List<String>>> conferenceMap = new HashMap<>();
+        conferenceMap.put("Western Conference", conferenceRepository::findWesternConferenceTeamNames);
+        conferenceMap.put("Eastern Conference", conferenceRepository::findEasternConferenceTeamNames);
+
+        List<Map<String, List<Ranking>>> finalResult = new ArrayList<>();
+
+        for (Map.Entry<String, Supplier<List<String>>> entry : conferenceMap.entrySet()) {
+            finalResult.add(Collections.singletonMap(entry.getKey(), processStandings(list, entry.getValue().get())));
+        }
+
+        return new DivisionRankings(season, finalResult);
+    }
+
+    public DivisionRankings getDivisionStandings(String season) {
+        List<StandingsPerSeason> list = standingsPerSeasonRepository.findBySeason(season);
+
+        Map<String, Supplier<List<String>>> divisionMap = new HashMap<>();
+        divisionMap.put("Atlantic", conferenceRepository::findAtlanticDivisionTeamNames);
+        divisionMap.put("Central", conferenceRepository::findCentralDivisionTeamNames);
+        divisionMap.put("Pacific", conferenceRepository::findPacificDivisionTeamNames);
+        divisionMap.put("SouthWest", conferenceRepository::findSouthWestDivisionTeamNames);
+        divisionMap.put("NorthWest", conferenceRepository::findNorthWestDivisionTeamNames);
+        divisionMap.put("SouthEast", conferenceRepository::findSouthEastDivisionTeamNames);
+
+        List<Map<String, List<Ranking>>> finalResult = new ArrayList<>();
+
+        for (Map.Entry<String, Supplier<List<String>>> entry : divisionMap.entrySet()) {
+            finalResult.add(Collections.singletonMap(entry.getKey(), processStandings(list, entry.getValue().get())));
+        }
+
+        return new DivisionRankings(season, finalResult);
+    }
+    private List<Ranking> processStandings(List<StandingsPerSeason> list, List<String> divisionTeams) {
+        return list.stream()
+                .filter(standings -> divisionTeams.contains(standings.getTeam()))
+                .sorted((s1, s2) -> {
+                    int wins1 = Integer.parseInt(s1.getOverall().split("-")[0]);
+                    int wins2 = Integer.parseInt(s2.getOverall().split("-")[0]);
+                    return Integer.compare(wins2, wins1);
+                })
+                .map(standings -> new Ranking(
+                        standings.getSeed(),
+                        standings.getTeam(),
+                        standings.getOverall()))
+                .collect(Collectors.toList());
+    }
+
 }
