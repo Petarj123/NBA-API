a279f783cdbd7e725b43125b93f41d4e6ad52ea8
Petarj123 <pjankovic03@gmail.com>
2023-07-19 12:51:02 +0200

    Added query method
    Queries players based on limit

--- nba/src/main/java/com/api/nba/leaders/AssistsRepository.java
+++ nba/src/main/java/com/api/nba/leaders/AssistsRepository.java
@@ -1,8 +1,14 @@
 package com.api.nba.leaders;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AssistsRepository extends JpaRepository<Assists, Long> {
+    @Query(value = "SELECT * FROM assists_leaders_alltime ORDER BY assists DESC LIMIT :limit", nativeQuery = true)
+    List<Assists> findTopPlayers(@Param("limit") Integer limit);
 }
--- nba/src/main/java/com/api/nba/leaders/StealsRepository.java
+++ nba/src/main/java/com/api/nba/leaders/StealsRepository.java
@@ -1,11 +1,14 @@
 package com.api.nba.leaders;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StealsRepository extends JpaRepository<Steals, Long> {
-    List<Steals> findAllByOrderByPlayerNameAsc();
+    @Query(value = "SELECT * FROM steals_leaders_alltime ORDER BY steals DESC LIMIT :limit", nativeQuery = true)
+    List<Steals> findTopPlayers(@Param("limit") Integer limit);
 }
