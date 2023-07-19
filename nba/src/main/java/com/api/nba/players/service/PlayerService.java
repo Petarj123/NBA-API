166f33ea864158fd0a3df281f1e883a2727dfea4
Petarj123 <pjankovic03@gmail.com>
2023-07-19 23:59:31 +0200

    Update PlayerService.java

--- nba/src/main/java/com/api/nba/players/service/PlayerService.java
+++ nba/src/main/java/com/api/nba/players/service/PlayerService.java
@@ -2,12 +2,18 @@ package com.api.nba.players.service;
 
 import com.api.nba.DTO.PlayerData;
 import com.api.nba.DTO.PlayerStat;
+import com.api.nba.players.model.AllPlayers;
 import com.api.nba.players.repository.PlayerRepository;
 import com.api.nba.players.model.Player;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -15,19 +21,38 @@ public class PlayerService {
 
     private final PlayerRepository playerRepository;
 
-    public PlayerData retrievePlayerSeasons(String firstName, String lastName){
+    public List<PlayerData> retrievePlayerSeasons(String firstName, String lastName){
         List<Player> players = playerRepository.findByFirstNameAndLastName(firstName, lastName);
-        List<PlayerStat> stats = players.stream().map(player -> new PlayerStat(player.getSeason(), player.getLeague(), player.getAge(),
-                player.getTeam(), player.getPosition(), player.getGamesPlayed(),
-                player.getGamesStarted(), player.getMinutesPerGame(), player.getFieldGoals(),
-                player.getFieldGoalAttempts(), player.getFieldGoalPercentage(), player.getThreePointFieldGoals(),
-                player.getThreePointFieldGoalAttempts(), player.getThreePointFieldGoalPercentage(), player.getTwoPointFieldGoals(),
-                player.getTwoPointFieldGoalAttempts(), player.getTwoPointFieldGoalPercentage(), player.getEffectiveFieldGoalPercentage(),
-                player.getFreeThrows(), player.getFreeThrowAttempts(), player.getFreeThrowPercentage(), player.getOffensiveRebounds(),
-                player.getDefensiveRebounds(), player.getTotalRebounds(), player.getAssists(), player.getSteals(), player.getBlocks(),
-                player.getTurnovers(), player.getPersonalFouls(), player.getPoints())
-        ).toList();
-        return new PlayerData(firstName + " " + lastName, stats);
+
+        // Group players by AllPlayers entity
+        Map<AllPlayers, List<Player>> playersGrouped = players.stream()
+                .collect(Collectors.groupingBy(Player::getAllPlayers));
+
+        // Create PlayerData for each group
+        List<PlayerData> playerDataList = new ArrayList<>();
+        for (Map.Entry<AllPlayers, List<Player>> entry : playersGrouped.entrySet()) {
+            AllPlayers allPlayer = entry.getKey();
+            List<Player> playersWithName = entry.getValue();
+
+            String name = allPlayer.getFirstName() + " " + allPlayer.getLastName();
+
+            List<PlayerStat> stats = playersWithName.stream().map(player -> new PlayerStat(player.getSeason(), player.getLeague(), player.getAge(),
+                    player.getTeam(), player.getPosition(), player.getGamesPlayed(),
+                    player.getGamesStarted(), player.getMinutesPerGame(), player.getFieldGoals(),
+                    player.getFieldGoalAttempts(), player.getFieldGoalPercentage(), player.getThreePointFieldGoals(),
+                    player.getThreePointFieldGoalAttempts(), player.getThreePointFieldGoalPercentage(), player.getTwoPointFieldGoals(),
+                    player.getTwoPointFieldGoalAttempts(), player.getTwoPointFieldGoalPercentage(), player.getEffectiveFieldGoalPercentage(),
+                    player.getFreeThrows(), player.getFreeThrowAttempts(), player.getFreeThrowPercentage(), player.getOffensiveRebounds(),
+                    player.getDefensiveRebounds(), player.getTotalRebounds(), player.getAssists(), player.getSteals(), player.getBlocks(),
+                    player.getTurnovers(), player.getPersonalFouls(), player.getPoints())
+            ).collect(Collectors.toList());
+
+            playerDataList.add(new PlayerData(name, stats));
+        }
+
+        return playerDataList;
     }
+
+
 
 }
