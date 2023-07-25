5ae3911456728345c34cc605b9fce806293fa2bb
Petarj123 <pjankovic03@gmail.com>
2023-07-25 18:26:40 +0200

    Create IllegalAbbreviationException.java

---
+++ nba/src/main/java/com/api/nba/exceptions/IllegalAbbreviationException.java
@@ -0,0 +1,7 @@
+package com.api.nba.exceptions;
+
+public class IllegalAbbreviationException extends Throwable {
+    public IllegalAbbreviationException(String message) {
+        super(message);
+    }
+}
