9d4c178f07668a801ad5b7bf55cd06d88843b540
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-10-21 18:43:23 +0200

    Update SeasonRepository.java

--- nba/src/main/java/com/api/nba/seasons/repository/SeasonRepository.java
+++ nba/src/main/java/com/api/nba/seasons/repository/SeasonRepository.java
@@ -4,9 +4,11 @@ import com.api.nba.seasons.model.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SeasonRepository extends JpaRepository<Season, Long> {
 
-    Season findBySeason(String season);
+    Optional<Season> findBySeason(String season);
 
 }
