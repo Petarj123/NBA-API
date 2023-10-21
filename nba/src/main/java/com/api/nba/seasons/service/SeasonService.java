8d05c57240aa91178a8a03ee26c513b8dc133271
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-10-21 18:43:28 +0200

    Update SeasonService.java

--- nba/src/main/java/com/api/nba/seasons/service/SeasonService.java
+++ nba/src/main/java/com/api/nba/seasons/service/SeasonService.java
@@ -7,6 +7,8 @@ import com.api.nba.seasons.repository.SeasonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class SeasonService {
@@ -14,73 +16,54 @@ public class SeasonService {
     private final SeasonRepository seasonRepository;
 
     public Champion getChampion(String season) throws InvalidSeasonException {
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new Champion(season, result.getChampion());
     }
+
     public PlayerAward getMvp(String season) throws InvalidSeasonException {
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new PlayerAward(season, result.getMvp());
     }
     public PlayerAward getRoy(String season) throws InvalidSeasonException {
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new PlayerAward(season, result.getRookieOfTheYear());
     }
     public PlayerAward getScoringChampion(String season) throws InvalidSeasonException {
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new PlayerAward(season, result.getPoints());
     }
     public PlayerAward getAssistsChampion(String season) throws InvalidSeasonException {
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new PlayerAward(season, result.getAssists());
     }
     public PlayerAward getReboundingChampion(String season) throws InvalidSeasonException{
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new PlayerAward(season, result.getRebounds());
     }
    public PlayerAward getPlayerWithHighestWinShares(String season) throws InvalidSeasonException {
-       Season result = seasonRepository.findBySeason(season);
-       if (result == null){
-           throw new InvalidSeasonException("The season " + season + "is not available");
-       }
+       Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
        return new PlayerAward(season, result.getWinShares());
    }
    public PlayerAward getDefensivePlayerOfTheYear(String season) throws InvalidSeasonException {
-       Season result = seasonRepository.findBySeason(season);
-       if (result == null){
-           throw new InvalidSeasonException("The season " + season + "is not available");
-       }
+       Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
        return new PlayerAward(season, result.getDefensivePlayerOfTheYear());
    }
    public PlayerAward getSixthManOfTheYear(String season) throws InvalidSeasonException {
-       Season result = seasonRepository.findBySeason(season);
-       if (result == null){
-           throw new InvalidSeasonException("The season " + season + "is not available");
-       }
+       Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
        return new PlayerAward(season, result.getSixthManOfTheYear());
    }
     public PlayerAward getMostImprovedPlayer(String season) throws InvalidSeasonException {
-        Season result = seasonRepository.findBySeason(season);
-        if (result == null){
-            throw new InvalidSeasonException("The season " + season + "is not available");
-        }
+        Season result = seasonRepository.findBySeason(season).orElseThrow(() -> new InvalidSeasonException("The season " + season + " is not available"));
+
         return new PlayerAward(season, result.getMostImproved());
     }
 }
